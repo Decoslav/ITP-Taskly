8d1cc793d1c3d9e02bc05d90227d7cf5c0453b4b
Paulartn <if24b296@technikum-wien.at>
2025-04-03 17:52:51 +0200

    Webseite1.1.0
    Funktionierendes Registrierung und Login Formular mit Datenbankverbindung sowie Logout

---
+++ ITP/Webseite1.1.0/db.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";
+$username = "root"; // Dein Datenbank-Benutzername
+$password = ""; // Dein Datenbank-Passwort (falls vorhanden)
+$dbname = "webseite_db"; // Name deiner Datenbank
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Verbindung fehlgeschlagen: " . $conn->connect_error);
+}
+?>
---
+++ ITP/Webseite1.1.0/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kalender</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <?php include 'header.php'; ?>
+</head>
+<body>
+<body class="container mt-5">
+    <h1>Kalender:</h1>
+    <img src="img/Kalender.png" alt="Beispiel Kalender" width="1100" height="600">
+
+</body>
+</html>
