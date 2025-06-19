a6536a06e9e73b490f5c4e3202febeabaecd9137
Vincent Leonardo Müller <if24b197@technikum-wien.at>
2025-06-19 15:39:51 +0200

    Login Reconfiguration, Include Folder

--- ITP/register.php
+++ ITP/register.php
@@ -42,7 +42,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registrierung</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <?php include 'header.php'; ?>
+    <?php include './includes/header.php'; ?>
 </head>
 <body class="container mt-5">
     <div class="row justify-content-center">
